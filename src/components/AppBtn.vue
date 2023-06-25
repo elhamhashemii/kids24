4fb12066d56661b19fa25426aa0b553f567d6333
Elham <hashemi.elham22@gmail.com>
2023-06-25 16:24:41 +0330

    feat: btn component added

---
+++ src/components/AppBtn.vue
@@ -0,0 +1,22 @@
+<template>
+    <button class="btn w-full h-full text-white rounded-md p-3 md:p-5 text-center text-xl" :class="`bg-${color}`" @click="onAction">{{ text }}</button>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface IProps {
+    text: string;
+    color: "violet" | "danger";
+}
+const props = withDefaults(defineProps<IProps>(), {
+    text: "defaultValue",
+    color: "violet"
+})
+const text = computed(() => props.text)
+const color = computed(() => props.color)
+const emits = defineEmits(['action'])
+function onAction() {
+    emits('action')
+}
+</script>
