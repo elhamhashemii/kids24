6db3ce8abd63aafcbff9b43a09a9b5c3aedc23ec
Elham <hashemi.elham22@gmail.com>
2023-06-25 16:25:00 +0330

    feat: menu component added

---
+++ src/components/AppMenu.vue
@@ -0,0 +1,19 @@
+<template>
+    <div
+        class="bg-white border border-secondary rounded-md px-4 py-2 flex items-center justify-between cursor-pointer">
+        <select class="w-full focus:outline-none">
+            <option v-for="item in items" :key="item" value="item">{{ item }}</option>
+            <ChevronDownIcon class="w-4 h-4" />
+        </select>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ChevronDownIcon } from '@heroicons/vue/24/solid';
+import { computed } from 'vue';
+interface IProps {
+    items: string[]
+}
+const props = defineProps<IProps>()
+const items = computed(() => props.items)
+</script>
