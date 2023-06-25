cfb3f255503a6319a91261d7bb9c4e5fca63f9ad
Elham <hashemi.elham22@gmail.com>
2023-06-25 15:37:58 +0330

    feat: appNav added

---
+++ src/components/layout/AppNav.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="bg-primary flex justify-end">
+        <div class="bg-primaryLight text-primary font-bold pt-6 px-4 pb-3"> 09:45 | 02 / 12 / 1401</div>
+        <div class="text-xl pt-6 px-4 pb-3 font-bold text-white">Kidz24</div>
+    </div>
+</template>
