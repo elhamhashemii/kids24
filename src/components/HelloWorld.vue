32d8a67c518bae6b64a8ddd067fea193c80f75b9
Elham <hashemi.elham22@gmail.com>
2023-06-25 13:04:55 +0330

    feat: iconPack added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,10 @@
 <template>
-  <div class="p-5 bg-primary text-white">سلام 2323ث</div>
+  <div class="p-5 bg-primary text-white">
+    <BeakerIcon class="h-6 w-6 text-primaryLight" />
+    سلام 124
+  </div>
 </template>
+
+<script setup>
+import { BeakerIcon } from '@heroicons/vue/24/solid'
+</script>
