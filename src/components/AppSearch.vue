dbac7a2a89f9bb1cf2e8d9b0b0785afbebbf58d4
Elham <hashemi.elham22@gmail.com>
2023-06-25 16:30:38 +0330

    imp: search component desgin improved

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -1,12 +1,10 @@
 <template>
-    <div class="relative bg-white border border-secondary rounded-md px-4 py-2">
-        <span class="absolute inset-y-0 left-0 flex items-center py-4">
-            <button type="submit" class="p-2 focus:outline-none focus:ring">
-                <MagnifyingGlassIcon class="w-6 h-6" />
-            </button>
-        </span>
+    <div class="relative bg-white border border-secondary rounded-md px-4 py-2 flex items-center justify-between">
         <input type="search" name="Search" placeholder="جستجو ..."
             class="w-full py-2 pl-10 text-md placeholder-dark rounded-md focus:outline-none" />
+        <button type="submit" class="focus:outline-none focus:ring">
+            <MagnifyingGlassIcon class="w-6 h-6" />
+        </button>
     </div>
 </template>
 
